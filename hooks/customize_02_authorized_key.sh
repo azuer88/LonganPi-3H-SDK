ec39e2269a926c132f9d1327d5892bf7f944fee6
Blue Cuenca <blue.cuenca@gmail.com>
2024-08-24 03:09:59 +0800

    fix customize 02 script
    Signed-off-by: Blue Cuenca <blue.cuenca@gmail.com>

--- hooks/customize_02_authorized_key.sh
+++ hooks/customize_02_authorized_key.sh
@@ -1,8 +1,8 @@
 # AUTHORIZED_KEY=  defined in .env
 # USER_NAME= defined in .env
 
-cat << EOF > "$1/home/$USER_NAME/.ssh/authorized_keys
+cat << EOF > "$1/home/$USER_NAME/.ssh/authorized_keys"
 $AUTHORIZED_KEY
 
 EOF
-chmod u+rw,o-rwx "$1/home/$USER_NAME/.ssh/authorized_keys
+chmod 0600 "$1/home/$USER_NAME/.ssh/authorized_keys"
