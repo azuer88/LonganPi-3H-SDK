cee3e6f28e0a5cc7bdd43353a1dafbc55018c6d9
Blue Cuenca <blue.cuenca@gmail.com>
2024-08-24 07:38:21 +0800

    change sipeed to var
    Signed-off-by: Blue Cuenca <blue.cuenca@gmail.com>

--- hooks/customize_00_base.sh
+++ hooks/customize_00_base.sh
@@ -1,15 +1,15 @@
-chroot "$1" useradd --home-dir /home/sipeed --create-home sipeed --shell /bin/bash
+chroot "$1" useradd --home-dir /home/$USER_NAME --create-home $USER_NAME --shell /bin/bash
 echo "Executing: echo $USER_NAME:$USER_PASS | chroot \"$1\" chpasswd"
 echo $USER_NAME:$USER_PASS | chroot "$1" chpasswd
-chroot "$1" usermod -a -G dialout sipeed
-chroot "$1" usermod -a -G cdrom sipeed
-chroot "$1" usermod -a -G audio sipeed
-chroot "$1" usermod -a -G video sipeed
-chroot "$1" usermod -a -G plugdev sipeed
-chroot "$1" usermod -a -G users sipeed
-chroot "$1" usermod -a -G netdev sipeed
-chroot "$1" usermod -a -G input sipeed
-chroot "$1" usermod -a -G sudo sipeed
+chroot "$1" usermod -a -G dialout $USER_NAME
+chroot "$1" usermod -a -G cdrom $USER_NAME
+chroot "$1" usermod -a -G audio $USER_NAME
+chroot "$1" usermod -a -G video $USER_NAME
+chroot "$1" usermod -a -G plugdev $USER_NAME
+chroot "$1" usermod -a -G users $USER_NAME
+chroot "$1" usermod -a -G netdev $USER_NAME
+chroot "$1" usermod -a -G input $USER_NAME
+chroot "$1" usermod -a -G sudo $USER_NAME
 echo root:root | chroot "$1" chpasswd
 sed -i -e s/workstation=no/workstation=yes/g "$1"/etc/avahi/avahi-daemon.conf
 echo U_BOOT_PARAMETERS=\"console=tty0 console=ttyS0,115200 rootwait earlycon clk_ignore_unused rw\" >> "$1"/etc/default/u-boot
