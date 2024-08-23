c2ff734329975a3feec5855df7e5e67024d669c7
Blue Cuenca <blue.cuenca@gmail.com>
2024-08-23 19:01:48 +0800

    update comments
    Signed-off-by: Blue Cuenca <blue.cuenca@gmail.com>

--- hooks/customize_01_wifi.sh
+++ hooks/customize_01_wifi.sh
@@ -1,18 +1,20 @@
-cat << EOF > "$1/etc/NetworkManager/system-connections/AzuerTEK_DSL.nmconnection
+# SSID=  load from .env (in mkrootfs.sh)
+# PKEY=  load from .env 
+
+cat << EOF > "$1/etc/NetworkManager/system-connections/$SSID.nmconnection"
 [connection]
-id=AzuerTEK_DSL
-uuid=eb2dc013-34bb-4708-9cd8-bc923925f301
+id=$SSID
+uuid=$(uuidgen)
 type=wifi
-interface-name=wlxe8519ee8f1a0
 
 [wifi]
 mode=infrastructure
-ssid=AzuerTEK_DSL
+ssid=$SSID
 
 [wifi-security]
 auth-alg=open
 key-mgmt=wpa-psk
-psk=r3VduykLwGAC
+psk=$PKEY
 
 [ipv4]
 method=auto
