d9381bbbdd4e3f2c2b09976983fcd4e5dd8f5f2b
Blue Cuenca <blue.cuenca@gmail.com>
2024-08-24 05:07:56 +0800

    create if missing .ssh folder
    Signed-off-by: Blue Cuenca <blue.cuenca@gmail.com>

--- hooks/customize_02_authorized_key.sh
+++ hooks/customize_02_authorized_key.sh
@@ -1,6 +1,9 @@
 # AUTHORIZED_KEY=  defined in .env
 # USER_NAME= defined in .env
 
+mkdir -p "$1/home/$USER_NAME/.ssh" 
+chmod 0700 "$1/home/$USER_NAME/.ssh"
+
 cat << EOF > "$1/home/$USER_NAME/.ssh/authorized_keys"
 $AUTHORIZED_KEY
 
