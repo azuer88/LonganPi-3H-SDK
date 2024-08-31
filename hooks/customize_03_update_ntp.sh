fd44aed17e745659c1d085ed017949f6f7d6e51f
Blue Cuenca <blue.cuenca@gmail.com>
2024-09-01 04:47:13 +0800

    print out modified timesyncd.conf
    Signed-off-by: Blue Cuenca <blue.cuenca@gmail.com>

--- hooks/customize_03_update_ntp.sh
+++ hooks/customize_03_update_ntp.sh
@@ -1,5 +1,11 @@
+TIMESYNCD_CONF="/etc/systemd/timesyncd.conf"
+
 if [ -n "$NTP_SERVER" ]; then
-    sed -i.bak -e "/\[Time\]/a NTP=$NTP_SERVER" "$1/etc/systemd/timesyncd.conf"
+    sed -i.bak -e "/\[Time\]/a NTP=$NTP_SERVER" "$1${TIMESYNCD_CONF}"
 else
     echo "NTP_SERVER not set, will not do anything."
 fi
+
+echo "*** $TIMESYNCD_CONF ***"
+cat "$1$TIMESYNCD_CONF"
+echo "***********************"
